7351476306fed6c97f230cea2f7e4fe8025808fd
koji kobayashi <kojikobayashi1840@gmail.com>
2017-09-15 13:00:46 +0900

    Add statusBox機能

--- app/src/js/app.js
+++ app/src/js/app.js
@@ -6,6 +6,7 @@ import "babel-polyfill";
 const TO_DO_APP = () => {
 	const stage = document.getElementById('stage');
 
+	// Model管理
 	const model = {
 		dispatcher: document.createElement('div'),
 		ev: new Event('dataChange'),
@@ -29,7 +30,8 @@ const TO_DO_APP = () => {
 			const task = {
 				content: form.content.value,
 				priority: form.priority.value,
-				limit: form.limit.value
+				limit: form.limit.value,
+				status: 'open'
 			};
 			model.setItem(task);
 			form.reset();
@@ -37,20 +39,90 @@ const TO_DO_APP = () => {
 	};
 
 	/**
+	 * 中身を空にする
+	 * @param {Node} target 中身を空にするオブジェクト
+	 */
+	const emptyHtml = (target) => {
+		while (target.firstChild) {
+			target.removeChild(target.firstChild);
+		}
+	};
+
+	/**
 	 * 画面の描画
 	 */
 	const render = () => {
+		// 全データ
 		const dataAll = model.getItem();
-		const ul = document.createElement('ul');
-		let html = '';
-		dataAll.forEach((v) => {
-			html += `<li>${v.content}<strong>優先度</strong>${v.priority}<strong>リミット</strong>${v.limit}</li>`;
-		});
-		ul.innerHTML = html;
-		while (stage.firstChild) {
-			stage.removeChild(stage.firstChild);
-		}
-		stage.appendChild(ul);
+
+		/**
+		 * タスクの描画
+		 */
+		const renderTask = () => {
+			const ul = document.createElement('ul');
+			let html = '';
+			dataAll.forEach((dataItem) => {
+				html += `
+					<li class="taskItem">
+						<p class="taskContent">${dataItem.content}</p>
+						<dl>
+							<dt>優先度</dt><dd>${dataItem.priority}</dd>
+						</dl>
+						<dl>
+							<dt>リミット</dt><dd>${dataItem.limit}</dd>
+						</dl>
+						<ul>
+							<li><label><input type="checkbox" class="js-complateItem">完了</label></li>
+							<li><button class="js-changeItem">編集</button></li>
+						</ul>
+					</li>
+				`;
+			});
+			ul.innerHTML = html;
+			emptyHtml(stage);
+			stage.appendChild(ul);
+		};
+
+		/**
+		 * ステータスの描画
+		 */
+		const renderStatus = () => {
+			class DataStatus {
+				constructor() {
+					this.dataAll = dataAll;
+					this.totalCount = this.dataAll.length;
+				}
+				complateCount() {
+					return this.totalCount - this.leftCount();
+				}
+				leftCount() {
+					let result = 0;
+					this.dataAll.forEach((value) => {
+						if (value.status === 'open') {
+							result += 1;
+						}
+					});
+					return result;
+				}
+				completionRate() {
+					return (this.complateCount() / this.totalCount) * 100;
+				}
+			}
+			const dataStatus = new DataStatus(dataAll);
+
+			const container = document.getElementById('js-statusBox');
+			// タスク総数
+			container.querySelector('.totalCount').textContent = dataStatus.totalCount;
+			// 残タスク数
+			container.querySelector('.leftCount').textContent = dataStatus.leftCount();
+			// 完了済み
+			container.querySelector('.leftCount').textContent = dataStatus.complateCount();
+			// 完遂率
+			container.querySelector('.completionRate').textContent = dataStatus.completionRate();
+		};
+
+		renderTask();
+		renderStatus();
 	};
 
 	// カスタムイベントのリスナー登録
